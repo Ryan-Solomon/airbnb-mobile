24f3719def11bca789d51691cfc76c12f6ee0aee
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-22 17:14:09 -0600

    fix resizeMode

--- src/screens/home-screen/HomeScreen.screen.tsx
+++ src/screens/home-screen/HomeScreen.screen.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import {
-  StyledBackground,
   StyledHomeScreenContainer,
+  StyledBackground,
 } from './HomeScreen.styles';
+import { ImageBackground, StyleSheet } from 'react-native';
 
 export const HomeScreen = () => {
   return (
     <StyledHomeScreenContainer>
-      <StyledBackground
+      <ImageBackground
+        style={styles.image}
         source={require('../../../assets/images/wallpaper.jpg')}
       />
     </StyledHomeScreenContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    resizeMode: 'cover',
+    height: '70%',
+  },
+});
--- src/screens/home-screen/HomeScreen.styles.tsx
+++ src/screens/home-screen/HomeScreen.styles.tsx
@@ -3,8 +3,3 @@ import styled from 'styled-components/native';
 export const StyledHomeScreenContainer = styled.View`
   flex: 1;
 `;
-
-export const StyledBackground = styled.ImageBackground`
-  height: 60%;
-  width: 100%;
-`;
