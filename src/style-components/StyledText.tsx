f0f6f798b9bf304823138ebbd63998bbf95ffd74
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-23 17:07:41 -0600

    add props to styled text and statically type check them

--- src/style-components/StyledText.tsx
+++ src/style-components/StyledText.tsx
@@ -3,14 +3,30 @@ import styled from 'styled-components/native';
 
 type StyledTextProps = {
   textColor: string;
+  fontSize: string;
+  textAlign: string;
+  margin: string;
 };
 
 const StyledText = styled.Text<StyledTextProps>`
   color: ${({ textColor }) => textColor};
+  font-size: ${({ fontSize }) => fontSize};
+  text-align: ${({ textAlign }) => textAlign};
+  margin: ${({ margin }) => margin};
 `;
 
 export const StyledTextComponent: FC<StyledTextProps> = ({
   textColor = 'black',
+  fontSize = '16px',
+  textAlign = 'center',
+  margin = '0px',
 }) => {
-  return <StyledText textColor={textColor} />;
+  return (
+    <StyledText
+      textColor={textColor}
+      fontSize={fontSize}
+      textAlign={textAlign}
+      margin={margin}
+    />
+  );
 };
